fded48b45114c8f36e8d60f55540345ac5cc99a5
Fenrich Alessandro <3001068@stud.hs-mannheim.de>
2025-05-05 09:04:35 +0200

    created stubs for bookRentalService

---
+++ bookRentalService/src/test/java/de/springBootExample/bookRentalService/BookRentalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.springBootExample.bookRentalService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRentalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
